44e7caed2127e362f47e0d8f1b8ddaa72f3b0f8a
SirPython <SirPython@github.noreply.com>
2020-02-02 15:55:44 -0500

    dot proudct works

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 #include "quantsim.h"
 
+#include <stdio.h>
+
 int main(int argc, char **argv) {
-    
+    struct Mat mat1;
+    Mat_init(&mat1, 2, 3);
+    Mat_set(&mat1, 0, 0, 1);
+    Mat_set(&mat1, 0, 1, 2);
+    Mat_set(&mat1, 0, 2, 3);
+    Mat_set(&mat1, 1, 0, 4);
+    Mat_set(&mat1, 1, 1, 5);
+    Mat_set(&mat1, 1, 2, 6);
+
+    struct Mat mat2;
+    Mat_init(&mat2, 3, 2);
+    Mat_set(&mat2, 0, 0, 7);
+    Mat_set(&mat2, 0, 1, 8);
+    Mat_set(&mat2, 1, 0, 9);
+    Mat_set(&mat2, 1, 1, 10);
+    Mat_set(&mat2, 2, 0, 11);
+    Mat_set(&mat2, 2, 1, 12);
+
+    struct Mat product;
+    Mat_mult(&mat1, &mat2, &product);
 }
--- mat.c
+++ mat.c
@@ -1,6 +1,7 @@
 #include "mat.h"
 
 #include <stdlib.h>
+#include <stdio.h>
 
 void Mat_init(struct Mat *m, int rows, int cols) {
     m->rows = rows;
@@ -18,7 +19,23 @@ void Mat_set(struct Mat *m, int row, int col, double val) {
 }
 
 void Mat_mult(struct Mat *m1, struct Mat *m2, struct Mat *result) {
+    if(m1->cols != m2->rows) {
+        return;
+    }
 
+    Mat_init(result, m1->rows, m2->cols);
+
+    for(int i = 0; i < m1->rows; i++) {
+        for(int j = 0; j < m2->cols; j++) {
+            double sum = 0;
+            for(int k = 0; k < m2->rows; k++) {
+                sum += Mat_get(m1, i, k) * Mat_get(m2, k, j);
+                printf("i: %d\nj: %d\nk: %d\nm1 v: %f\nm2 v: %f\n==========\n", i, j, k, Mat_get(m1, i, k), Mat_get(m2, k, j));
+            }
+
+            Mat_set(result, i, j, sum);
+        }
+    }
 }
 
 void Mat_tensor(struct Mat *m1, struct Mat *m2, struct Mat *result) {
